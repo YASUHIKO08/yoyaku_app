59168c2eae30667cd9135f1b2c844c380faab7a6
yasuhiko <m0922vvv@gmail.com>
2023-08-24 18:08:35 +0900

    登録施設、予約人数訂正

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,10 +3,17 @@ class Reservation < ApplicationRecord
   validates :check_out, presence: true
   validates :people, presence: true
   validate :before_date
+  validate :people_min
 
   def before_date
     if check_in.present? && check_out.present? && check_in > check_out
       errors.add(:check_in, "をチェックアウト以降の日付で指定することは出来ません")
+    end
+  end
+
+  def people_min
+    if people.present? && people < 1
+    errors.add(:people,"は１以上の数値を入力してください")
     end
   end
 
